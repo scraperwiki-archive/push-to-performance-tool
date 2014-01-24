bf6a59e75c45b7df8e3526220559cc35c24bbed0
Zarino Zappia <mail@zarino.co.uk>
2014-01-24 12:10:39 +0000

    move code and tests into new directory

---
+++ code/test.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python
+
+import unittest
+import mock
+
+# The functions we want to test
+# are in ./update.py
+from update import download_data
+
+
+# This gets used in our tests to fake some API calls
+class Dispenser(object):
+    def __init__(self, items):
+        self.items = items
+
+    def __call__(self, *args, **kwargs):
+        return self.items.pop(0)
+
+
+class TestDataDownloader(unittest.TestCase):
+    def setUp(self):
+        self.box_url = 'http://example.com/foo/bar'
+
+    @mock.patch('requests.get')
+    def test_requesting_data(self, mock_requests_get):
+        download_data(self.box_url)
+        url = '{0}/sql?q=select * from swdata limit 5000 offset 0'.format(self.box_url)
+        mock_requests_get.assert_called_with(url)
+
+    @mock.patch('requests.get')
+    def test_decoding_requested_data(self, mock_requests_get):
+        mock_requests_get.return_value = response = mock.Mock()
+
+        # Tell responseObject.json() what to return
+        response.json.return_value = [{1: 2}]
+
+        # Try it out, with our mocked responseObject
+        d = download_data(self.box_url)
+
+        self.assertEqual(d, [{1: 2}])
+
+    @mock.patch('requests.get')
+    def test_downloads_multiple_chunks(self, mock_requests_get):
+        mock_requests_get.return_value = response = mock.Mock()
+
+        # Tell responseObject.json() to return
+        # `first_response` the first time it's called and
+        # `second_response` the second time it's called.
+        first_response = [{1: 2}] * 5000
+        second_response = [{1: 2}]
+        response.json.side_effect = Dispenser([first_response, second_response])
+
+        d = download_data(self.box_url)
+
+        url = '{0}/sql?q=select * from swdata limit 5000 offset 5000'.format(self.box_url)
+        mock_requests_get.assert_called_with(url)
+
+        self.assertEqual(d, [{1: 2}] * 5001)
+
+if __name__ == '__main__':
+   unittest.main()
---
+++ code/update.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+
+import datetime
+import requests
+import sys
+import itertools
+
+import scraperwiki.runlog; scraperwiki.runlog.setup()
+
+
+def main():
+    # Get the data from the dataset
+    if len(sys.argv) < 2:
+        # write error message
+        return
+
+    raise RuntimeError("Something went wrong")
+
+    box_url = sys.argv[1]
+    data = download_data(box_url)
+
+    from pprint import pprint
+    pprint(data)
+
+    # Upload data to performance platform
+    result = magic_upload_to_gds(where, data)
+
+    # Write upload date to index
+    if result != "all good":
+        scraperwiki.status("error", "data failed to upload")
+
+
+def download_data(box_url):
+    all_results = []
+    for offset in itertools.count(0, 5000):
+        url = '{0}/sql?q=select * from swdata limit 5000 offset {1}'.format(box_url, offset)
+        chunk = requests.get(url).json()
+        all_results.extend(chunk)
+        if len(chunk) < 5000:
+            break
+    return all_results
+
+
+if __name__ == '__main__':
+   main()
