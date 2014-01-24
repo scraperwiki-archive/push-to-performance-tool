3a161eadf2ffad56c86d76dc3a4d76c441ea3adc
Tom Mortimer-Jones <tom@morty.co.uk>
2014-01-24 17:12:59 +0000

    Save settings to allSettings.json.

--- http/code.js
+++ http/code.js
@@ -1,3 +1,29 @@
-$(function(){
+var saveSettings = function(e) {
+  e.preventDefault()
 
+  var url = $('#url').val()
+  var token = $('#token').val()
+
+  $('#save').addClass('loading disabled').html('Saving&hellip;')
+
+  $.ajax({
+    url: '../cgi-bin/save-settings'
+  , data: {
+      url: url
+    , token: token
+    }
+  , dataType: 'json'
+  }).done(function(data) {
+    $('#save').removeClass('loading disabled').html('Save')
+    $('#saved').show()
+    setTimeout(function() {
+      $('#saved').fadeOut()
+    }, 4000)
+  }).fail(function(jqXHR, textStatus, errorThrown) {
+    $('#save').removeClass('loading disabled').html('Save')
+  })
+}
+
+$(function(){
+  $('#settings').on('submit', saveSettings)
 })
